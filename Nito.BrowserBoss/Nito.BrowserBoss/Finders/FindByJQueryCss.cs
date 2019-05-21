24200658f7b9f9cf6835adc698810e04e40e68b0
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-05-20 21:23:24 -0400

    Remove SizSelCsZzz dependency.

--- Nito.BrowserBoss/Nito.BrowserBoss/Finders/FindByJQueryCss.cs
+++ Nito.BrowserBoss/Nito.BrowserBoss/Finders/FindByJQueryCss.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
 using OpenQA.Selenium;
-using SizSelCsZzz;
 
 namespace Nito.BrowserBoss.Finders
 {
@@ -9,9 +13,68 @@ namespace Nito.BrowserBoss.Finders
     /// </summary>
     public sealed class FindByJQueryCss : IFind
     {
+        public string JQueryUri { get; set; } = "https://code.jquery.com/jquery-3.4.1.slim.min.js";
+
         IReadOnlyCollection<IWebElement> IFind.Find(ISearchContext context, string searchText)
         {
-            return context.FindElements(ByJQuery.CssSelector(searchText));
+            return context.FindElements(new ByJQuery(JQueryUri, searchText));
+        }
+
+        private sealed class ByJQuery : By
+        {
+            private readonly string _jQueryUri;
+            private readonly string _selector;
+
+            public ByJQuery(string jQueryUri, string selector)
+            {
+                _jQueryUri = jQueryUri;
+                _selector = JsonConvert.SerializeObject(selector);
+                Description = $"ByJQuery: {selector}";
+            }
+
+            public override ReadOnlyCollection<IWebElement> FindElements(ISearchContext context)
+            {
+                var scriptExecutor = (IJavaScriptExecutor)context;
+                EnsureJQueryIsLoaded(scriptExecutor);
+
+                IEnumerable<object> result;
+                if (context is IWebElement)
+                    result = (IEnumerable<object>) scriptExecutor.ExecuteScript($"return jQuery.makeArray(jQuery({_selector}, arguments[0]))", context);
+                else
+                    result = (IEnumerable<object>) scriptExecutor.ExecuteScript($"return jQuery.makeArray(jQuery({_selector}))");
+
+                return new ReadOnlyCollection<IWebElement>(result.Cast<IWebElement>().ToList());
+            }
+
+            public override IWebElement FindElement(ISearchContext context)
+            {
+                var found = FindElements(context);
+                if (found.Count == 0)
+                    throw new NoSuchElementException($"Could not find element matching jQuery selector {_selector}.");
+
+                return found[0];
+            }
+
+            void EnsureJQueryIsLoaded(IJavaScriptExecutor scriptExecutor)
+            {
+                if (IsJQueryLoaded(scriptExecutor))
+                    return;
+
+                var script = new WebClient().DownloadString(_jQueryUri);
+                scriptExecutor.ExecuteScript(script);
+            }
+
+            bool IsJQueryLoaded(IJavaScriptExecutor scriptExecutor)
+            {
+                try
+                {
+                    return (bool) scriptExecutor.ExecuteScript("return typeof(jQuery)==='function'");
+                }
+                catch
+                {
+                    return false;
+                }
+            }
         }
     }
 }
